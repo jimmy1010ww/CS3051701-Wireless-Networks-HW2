3954bac8f21d9359df48a5b0148ebc4d6c974828
jimmy1010ww <35855869+jimmy1010ww@users.noreply.github.com>
2022-11-28 20:23:21 +0800

    20221128

--- client/client.py
+++ client/client.py
@@ -24,7 +24,7 @@ def getCalcMode():
         print("2 -> Read from file")
         print("q -> Quit")
 
-        user_input = input('$')
+        user_input = input('$ ')
         if user_input == '1':
             return '1'
         elif user_input == '2':
@@ -34,56 +34,128 @@ def getCalcMode():
         else:
             print("Invalid input")
 
+def fileRecv(fileName, socket):
+    #打開要接收文件
+    with open(os.getcwd() + '/' + fileName,'wb') as f:
+        #開始接收文件
+        while True:
+            print ("Receiving...")
+            data = socket.recv(1024)
+            f.write(data)
+            #如果檔案長度不夠1024則表示接收完畢
+            if len(data) < 1024:
+                break
+        #檔案關閉
+        f.close()
+    print('receive done')
+
+def fileSend(fileName, socket):
+    #打開要傳送的文件
+    with open(os.getcwd() + '/' + fileName,'rb') as f:
+        #開始傳送文件
+        l = f.read(1024)
+        #如果檔案長度不夠1024則表示傳送完畢
+        while (l):
+            print ('Sending...')
+            socket.send(l)
+            l = f.read(1024)
+        #檔案關閉
+        f.close()
+    print('send done.')
 
 if __name__ == "__main__":
     os.system("clear")
 
+    #獲取與伺服器的socket
     client_socket = initClientSocket()
 
-    while True:
-        send_mode = getCalcMode()
-        print('Choose mode => ' + send_mode)
+    try:
+        while True:
+            #取得計算機的模式
+            send_mode = getCalcMode()
+            print('Choose mode => ' + send_mode)
 
-        if send_mode == 'q' or send_mode == 'Q':
-            client_socket.send("q".encode("utf-8"))
-            print("Bye~")
-            quit()
-        else:
-            client_socket.send(send_mode.encode("utf-8"))
-            calc_mode = int(send_mode)
-    
-        indata = client_socket.recv(1024)
-        if len(indata) == 0: # connection closed
-            client_socket.close()
-            print('server closed connection.')
-            break
-        elif indata == 'OK':
-            print('Server ready to receive data.')
-
-
+            #如果選擇離開則離開
+            if send_mode == 'q' or send_mode == 'Q':
+                client_socket.send("q".encode("utf-8"))
+                print("Bye~")
+                quit()
+            else:
+                client_socket.send(send_mode.encode("utf-8"))
+                calc_mode = int(send_mode)
         
-        if calc_mode == 1:
-            while True:
-                print("Please input the calculation expression, or input 'q' to quit.")
-                user_input = input('$')
-                if user_input == 'q' or user_input == 'Q':
-                    client_socket.send("MODE_QUIT".encode("utf-8"))
-                    calc_mode = 0
-                    break
-                else:
-                    client_socket.send(user_input.encode("utf-8"))
-                    indata = client_socket.recv(1024)
-                    print('ANS => ' + indata.decode("utf-8"))
-        elif calc_mode == 2:
-            with open(os.getcwd() + '/' + INPUT_FILE_NAME,'rb') as f:
-                l = f.read(1024)
-                while (l):
-                    print ('Sending...')
-                    client_socket.send(l)
-                    l = f.read(1024)
-                f.close()
+            #接收伺服器回應
+            indata = client_socket.recv(1024)
 
-            print('send done.')
+            #連線關閉
+            if len(indata) == 0:
+                client_socket.close()
+                print('server closed connection.')
+                break
+            #伺服器回應 OK
+            elif indata == 'OK':
+                print('Server ready to receive data.')
+
+
+            #根據計算機模式執行
+            if calc_mode == 1:  #單行執行
+                while True:
+                    print("Please input the calculation expression, or input 'q' to quit.")
+                    user_input = input('$ ')
+
+                    #如果輸入q回到選擇模式（預設)
+                    if user_input == 'q' or user_input == 'Q':
+                        client_socket.send("MODE_QUIT".encode("utf-8"))
+                        calc_mode = 0
+                        break
+                    else:
+                        #傳送輸入的計算式到伺服器端
+                        client_socket.send(user_input.encode("utf-8"))
+                        #接收伺服器端的回應
+                        indata = client_socket.recv(1024)
+                        indata = indata.decode("utf-8")
+
+                        #如果伺服器回應為 'Invalid input' 則重新輸入
+                        if indata == "Invalid input":
+                            print('Invalid input')
+                            continue
+                        else:
+                        #反之輸出伺服器端的回應
+                            print('ANS => {:.2f}'.format(float(indata)))
+            #檔案傳輸模式
+            elif calc_mode == 2:
+                #傳送 Testcase.txt
+                fileSend(INPUT_FILE_NAME, client_socket)
+
+                #等待伺服器回傳 ans.txt，超過 10 秒則離開
+                client_socket.settimeout(10)
+                try:
+                    #接收伺服器回應的訊號
+                    signal = client_socket.recv(1024)
+                    signal = signal.decode("utf-8")
+
+                    #開始進行檔案傳輸
+                    if signal == 'OK':
+                        fileRecv(ANS_FILE_NAME, client_socket)
+
+                #逾時處理
+                except socket.timeout:
+                    print('Server not responce.')
+                    break
+
+                #顯示出 ans.txt 的內容
+                f = open(os.getcwd() + '/' + ANS_FILE_NAME, 'r')
+                for lines in f:
+                    print(lines, end='')
+                f.close()
+                calc_mode = 0
+    #keyboard interrupt處理
+    except KeyboardInterrupt:
+        print("\nBye~")
+        client_socket.close()
+        quit()
+
+
 
 
 
--- server/server.py
+++ server/server.py
@@ -19,74 +19,128 @@ def initServerSocket():
     print (f"Listening on Ip:{HOST}\nPort:{PORT}\n===================================")
     return s
 
+def fileRecv(fileName, socket):
+    #打開要接收文件
+    with open(os.getcwd() + '/' + fileName,'wb') as f:
+        #開始接收文件
+        while True:
+            print ("Receiving...")
+            data = socket.recv(1024)
+            f.write(data)
+            #如果檔案長度不夠1024則表示接收完畢
+            if len(data) < 1024:
+                break
+        #檔案關閉
+        f.close()
+    print('receive done')
+
+def fileSend(fileName, socket):
+    #打開要傳送的文件
+    with open(os.getcwd() + '/' + fileName,'rb') as f:
+        #開始傳送文件
+        l = f.read(1024)
+        #如果檔案長度不夠1024則表示傳送完畢
+        while (l):
+            print ('Sending...')
+            socket.send(l)
+            l = f.read(1024)
+        #檔案關閉
+        f.close()
+    print('send done.')
+
+def doCalc(expression):
+    try:
+        expression = expression.replace("^","**")
+        Ans=str(eval(expression))
+    except:
+        Ans='Invalid input'
+    return Ans
 
 
 if __name__ == "__main__":
     os.system("clear")
 
-    server_cocket = initServerSocket()
+    try:
 
-    while True:
-        print("Wait for connection...")
-        client,addr = server_cocket.accept()
-        print (f"Connected from: {addr}")
-
+        server_cocket = initServerSocket()
+        
         while True:
+            print("Wait for connection...")
+            client,addr = server_cocket.accept()
+            print (f"Connected from: {addr}")
 
-            # 初始化狀態，準備接受用戶端模式資料
-            if calc_mode == 0:
-                #接收用戶端傳送過來的資料
-                recv_data = client.recv(1024)
-                #用 UTF-8 解碼
-                decode_data = recv_data.decode("utf-8")
+            with client:
+                while True:
 
-                #處理不正確發送請求
-                if decode_data.isnumeric():
-                    calc_mode = int(decode_data)
-                elif decode_data == 'q' or decode_data == 'Q':
-                    print("Client quit.")
-                    client.close()
-                    print("\n===================================")
-                    break
-                else:
-                    print(f"Client sent invalid data => {decode_data}")
-                    continue
+                    # 初始化狀態，準備接受用戶端模式資料
+                    if calc_mode == 0:
+                        #接收用戶端傳送過來的資料
+                        recv_data = client.recv(1024)
+                        #用 UTF-8 解碼
+                        decode_data = recv_data.decode("utf-8")
 
-                #回傳 OK 給用戶端，表示伺服器準備好接受資料
-                print(f"Client choose mode: {calc_mode}")
-                client.send("OK".encode("utf-8"))
-
-            # 模式 1: 手動（單行）輸入
-            elif calc_mode == 1:
-                recv_data = client.recv(1024)
-                decode_data = recv_data.decode("utf-8")
-
-                if decode_data == 'MODE_QUIT':
-                    print("Exit mode 1.")
-                    calc_mode = 0
-                    continue
-                else:
-                    print(f"Client sent data => {decode_data}")
-
-                    try:
-                        decode_data=decode_data.replace("^","**")
-                        Ans=str(eval(decode_data))
-                    except:
-                        Ans='NULL'
-                    print('ANS => '+Ans)
-                    client.send(Ans.encode("utf-8"))
-            elif calc_mode == 2:
-                print(os.getcwd() + '/' + INPUT_FILE_NAME)
-                with open(os.getcwd() + '/' + INPUT_FILE_NAME,'wb') as f:
-                    while True:
-                        print ("Receiving...")
-                        l = client.recv(1024)
-                        f.write(l)
-                        if len(l) < 1024:
+                        #處理不正確發送請求
+                        if decode_data.isnumeric():
+                            calc_mode = int(decode_data)
+                        elif decode_data == 'q' or decode_data == 'Q':
+                            print("Client quit.")
+                            client.close()
+                            print("\n===================================")
                             break
-                    f.close()
-                print('receive done')
-                calc_mode = 0
+                        else:
+                            print(f"Client sent invalid data => {decode_data}")
+                            continue
+
+                        #回傳 OK 給用戶端，表示伺服器準備好接受資料
+                        print(f"Client choose mode: {calc_mode}")
+                        client.send("OK".encode("utf-8"))
+
+                    # 模式 1: 手動（單行）輸入
+                    elif calc_mode == 1:
+                        recv_data = client.recv(1024)
+                        decode_data = recv_data.decode("utf-8")
+
+                        if decode_data == 'MODE_QUIT':
+                            print("Exit mode 1.")
+                            calc_mode = 0
+                            continue
+                        else:
+                            print(f"Client sent data => {decode_data}")
+                            Ans = doCalc(decode_data)
+                            if Ans == "Invalid input" or Ans == '':
+                                print("Client sent invalid data.")
+                                client.send("Invalid input".encode("utf-8"))
+                            else:
+                                print("Server sent data => {:.2f}".format(float(Ans)))
+                                client.send(doCalc(Ans).encode("utf-8"))
+
+                    elif calc_mode == 2:
+                        #接收 Testcase.txt
+                        fileRecv(INPUT_FILE_NAME, client)
+
+                        #計算
+                        f_input = open(INPUT_FILE_NAME, 'r')
+                        f_ans = open(ANS_FILE_NAME, 'w')
+
+                        for lines in f_input:
+                            f_ans.write(doCalc(lines) + '\n')
+                        
+                        f_input.close()
+                        f_ans.close()
+
+                        client.send("OK".encode("utf-8"))
+                        fileSend(ANS_FILE_NAME, client)
+
+                        print("Exit mode 2")
+                        calc_mode = 0
+    except KeyboardInterrupt:
+        print("Server quit.")
+        client.close()
+    except:
+        print("Server error.")
+        quit()
+
+
 
 
 
